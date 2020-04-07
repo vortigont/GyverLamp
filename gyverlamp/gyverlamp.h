4ecdff06f7ba1637aacd0b71c9b21e76a3a625d2
Emil Muratov <gpm@hotplug.ru>
2020-04-07 23:34:18 +0300

    NTP: obsolete NTPClient lib
    Убрана зависимость от внешней библиотеки NTPClient.
    Для синхронизации c ntp и работой с датой/временем используются фунции ядра
     - поддерживается установка ntp-сервера из опций DHCP-аренды
     - автонастройка правил летнего времени в зависимости от временной зоны
     - временная зона устанавливается на основе определений из TZ.h
     - в конфиге задается двухбуквенный код страны для автовыбора ближайшего NTP-сервера по-умолчанию
    
    Будильник:
     - будильник блокируется если время не было выставленно через ntp или потеряно соединение с WiFi
     - костыли с обработкой даты/веремени переписаны на фунции ядра
    
    Signed-off-by: Emil Muratov <gpm@hotplug.ru>

--- gyverlamp/gyverlamp.h
+++ gyverlamp/gyverlamp.h
@@ -8,4 +8,11 @@
 #define DEFAULT_BRIGHNESS   50
 #define DEFAULT_SPEED       30
 #define DEFAULT_SCALE       40
-#define DEFAILT_TIMER       60000   //msec
+#define DEFAULT_TIMER       60000   //msec
+
+#define NTP_SERVER COUNTRY ".pool.ntp.org"
+
+// NTP time
+#include <coredecls.h>                  // settimeofday_cb()
+#include <TZ.h>
+#include <sntp.h>
